94bbe4876a21207ad0fbc7deb6b8e70d1cfd42b6
Ahmad Wael <ahmadwael2004@gmail.com>
2023-08-26 01:51:14 +0300

    Added IR sensors to the code
    Added IR sensors to detect white lines to the code

---
+++ The_Pythons_Arduino_Mega_Code/IR_Functions.ino
@@ -0,0 +1,28 @@
+void ir_read()
+{
+  ir_read_FR = analogRead(ir_FR);
+  ir_read_RL = analogRead(ir_RL);
+  ir_read_LR = analogRead(ir_LR);
+  ir_read_LL = analogRead(ir_LL);
+  ir_read_BL = analogRead(ir_BL);
+  ir_read_BR = analogRead(ir_BR);
+  ir_read_RR = analogRead(ir_RR);
+  ir_read_FL = analogRead(ir_FL);
+
+  Serial.print("IR_FR: ");
+  Serial.print(ir_read_FR);
+  Serial.print("\tIR_RL: ");
+  Serial.print(ir_read_RL);
+  Serial.print("\tIR_LR: ");
+  Serial.print(ir_read_LR);
+  Serial.print("\tIR_LL: ");
+  Serial.print(ir_read_LL);
+  Serial.print("\tIR_BL: ");
+  Serial.print(ir_read_BL);
+  Serial.print("\tIR_BR: ");
+  Serial.print(ir_read_BR);
+  Serial.print("\tIR_RR: ");
+  Serial.print(ir_read_RR);
+  Serial.print("\tIR_FL: ");
+  Serial.println(ir_read_FL);
+}
--- The_Pythons_Arduino_Mega_Code/The_Pythons_Arduino_Mega_Code.ino
+++ The_Pythons_Arduino_Mega_Code/The_Pythons_Arduino_Mega_Code.ino
@@ -9,8 +9,25 @@
 #define r_f_dir 10 
 #define r_f_pwm 11 
 
-
 #define move_speed 50
+
+#define ir_FR A0
+#define ir_RR A1
+#define ir_LR A2
+#define ir_LL A3
+#define ir_BL A4
+#define ir_BR A5
+#define ir_RL A6
+#define ir_FL A7
+
+int ir_read_FR = 0;
+int ir_read_RL = 0;
+int ir_read_LR = 0;
+int ir_read_LL = 0;
+int ir_read_BL = 0;
+int ir_read_BR = 0;
+int ir_read_RR = 0;
+int ir_read_FL = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -23,40 +40,42 @@ void setup() {
   pinMode(r_b_pwm, OUTPUT);
   pinMode(r_f_dir, OUTPUT);
   pinMode(r_b_dir, OUTPUT);
+
+  pinMode(ir_FR, INPUT);
+  pinMode(ir_RL, INPUT);
+  pinMode(ir_LR, INPUT);
+  pinMode(ir_LL, INPUT);
+  pinMode(ir_BL, INPUT);
+  pinMode(ir_BR, INPUT);
+  pinMode(ir_RR, INPUT);
+  pinMode(ir_FL, INPUT);
+  delay(3000);
   
 }
 
 void loop() {
-//  digitalWrite(l_f_dir, LOW);//counter-clock//left back
-//  analogWrite(l_f_pwm, move_speed);
-//  digitalWrite(l_b_dir, LOW);//counter-clock//left front
-//  analogWrite(l_b_pwm, move_speed);
-//
-//  digitalWrite(r_f_dir, HIGH);//clock//right front
-//  analogWrite(r_f_pwm, move_speed);
-//  digitalWrite(r_b_dir, HIGH);//clock//right back
-//  analogWrite(r_b_pwm, move_speed);
-  delay(5000);
-  moveWheels('f');//forward
-  delay(2000);
-  moveWheels('b');//backward
-  delay(2000);
-  moveWheels('r');//right
-  delay(2000);
-  moveWheels('l');//back
-  delay(2000);
-  moveWheels('y');//diagonal forward right
-  delay(2000);
-  moveWheels('h');//diagonal backward right
-  delay(2000);
-  moveWheels('t');//diagonal forward left
-  delay(2000);
-  moveWheels('g');//diagonal backward left
-  delay(2000);
-  moveWheels('m');//rotate right
-  delay(2000);
-  moveWheels('n');//rotate left
-  delay(2000);
-  moveWheels('s');//stop
-  delay(2000);
+  
+//  delay(5000);
+//  moveWheels('f');//forward
+//  delay(2000);
+//  moveWheels('b');//backward
+//  delay(2000);
+//  moveWheels('r');//right
+//  delay(2000);
+//  moveWheels('l');//back
+//  delay(2000);
+//  moveWheels('y');//diagonal forward right
+//  delay(2000);
+//  moveWheels('h');//diagonal backward right
+//  delay(2000);
+//  moveWheels('t');//diagonal forward left
+//  delay(2000);
+//  moveWheels('g');//diagonal backward left
+//  delay(2000);
+//  moveWheels('m');//rotate right
+//  delay(2000);
+//  moveWheels('n');//rotate left
+//  delay(2000);
+//  moveWheels('s');//stop
+//  delay(2000);
 }
