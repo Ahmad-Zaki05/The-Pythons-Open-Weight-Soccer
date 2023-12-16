4eb30e1c9202aee940e53876407c64248f76cda1
Ahmad-Zaki05 <107484564+Ahmad-Zaki05@users.noreply.github.com>
2023-12-16 13:25:56 +0200

    Update The_Pythons_Openmv_Code.py

--- The_Pythons_Openmv_Code/The_Pythons_Openmv_Code.py
+++ The_Pythons_Openmv_Code/The_Pythons_Openmv_Code.py
@@ -7,30 +7,35 @@ sensor.skip_frames(time = 2000)
 sensor.set_auto_gain(False)
 sensor.set_auto_whitebal(False)
 sensor.set_vflip(True)
-#orangeThreshold = [(53, 100, -13, 65, 34, 74), (49, 88, 27, 71, 17, 52)]#(36, 88, 29, 71, -6, 52)
-orangeThreshold = [(36, 88, 29, 71, -6, 52)]#(49, 88, 27, 71, 17, 52)
+orangeThreshold = [(37, 67, 41, 98, 14, 96), (25, 67, 39, 112, 18, 103), (37, 76, 28, 56, -2, 32)] # (36, 88, 29, 71, -6, 52)
+yellowThreshold = [(32, 39, 10, 31, 32, 46), (30, 45, 18, 42, 30, 54)]
 uart = UART(3, 115200)
-camcy = 127
-camcx = 152
-dribcx = 151
-dribcy = 53
+camcy = 122
+camcx = 136
+dribcx = 132
+dribcy = 65
+flagBall = 0
+lastAngle = 0
+lastRadius = 0
 clock = time.clock()
 while(True):
     clock.tick()
     img = sensor.snapshot()
-    img.draw_circle(camcx, camcy, 50, color=(0,0,0), thickness=1, fill=True)
-    img.draw_circle(camcx, camcy, 140, color=(0,0,0), thickness=55, fill=False)
-    img.draw_circle(camcx, camcy, 190, color=(0,0,0), thickness=50, fill=False)#temporary
+#    img.draw_circle(camcx, camcy, 50, color=(0,0,0), thickness=1, fill=True)
+#    img.draw_circle(camcx, camcy, 140, color=(0,0,0), thickness=55, fill=False)
+#    img.draw_circle(camcx, camcy, 190, color=(0,0,0), thickness=50, fill=False)
     img.draw_cross(dribcx, dribcy)
     orangeBlobs = img.find_blobs(orangeThreshold, pixels_threshold=17, area_threshold=17, merge=True)
+    yellowBlobs = img.find_blobs(yellowThreshold, pixels_threshold=20, area_threshold=20, merge=True)
     if orangeBlobs:
         ballBlob = max(orangeBlobs, key=lambda bL: bL.area())
         img.draw_edges(ballBlob.min_corners(), color=(255, 255, 255))
         dribAngle = (math.atan2((ballBlob.cy() - dribcy), (ballBlob.cx() - dribcx)) * 180 / math.pi) + 180
         cAngle = (math.atan2((ballBlob.cy() - camcy), (ballBlob.cx() - camcx)) * 180 / math.pi) + 180
         bRadius = (math.sqrt(((ballBlob.cy() - camcy) * (ballBlob.cy() - camcy)) + ((ballBlob.cx() - camcx) * (ballBlob.cx() - camcx))))
+        lastAngle = cAngle
+        lastRadius = bRadius
         print("bRadius: ", bRadius)
-
         if (cAngle >= 80 and cAngle <= 100):
             uart.write("B" + str(int(cAngle)) + "\n")
         elif(cAngle >= 0 and cAngle <= 90 and bRadius < 75):
@@ -49,4 +54,25 @@ while(True):
             uart.write("B" + str(int(dribAngle)) + "\n")
         print("dribAngle: ", dribAngle)
     else:
-        uart.write("S\n")
+        if flagBall:
+            if yellowBlobs:
+                goalBlob = max(yellowBlobs, key=lambda bL: bL.area())
+                img.draw_edges(goalBlob.min_corners(), color=(255, 255, 255))
+                goalAngle = (math.atan2((goalBlob.cy() - camcy), (goalBlob.cx() - camcx)) * 180 / math.pi) + 180
+                goalRadius = (math.sqrt(((goalBlob.cy() - camcy) * (goalBlob.cy() - camcy)) + ((goalBlob.cx() - camcx) * (goalBlob.cx() - camcx))))
+                print("goalAngle: ", goalAngle)
+                print("goal radius: ", goalRadius)
+                if(goalRadius < 100):
+                    uart.write("H" + "\n")
+                    print("shooot")
+                else:
+                    uart.write("G" + str(int(goalAngle)) + "\n")
+        else:
+            print("S")
+            uart.write("S\n")
+    print("last radius: ", lastRadius)
+    print("last angle: ", lastAngle)
+    if(lastRadius < 75 and lastAngle > 80 and lastAngle < 100):
+        flagBall = 1
+    else:
+        flagBall = 0
