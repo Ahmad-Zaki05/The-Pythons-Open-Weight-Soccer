6ac2b1bc2bd93cd0aac82a6c727d63879751da93
Ahmad Wael <ahmadwael2004@gmail.com>
2024-02-09 19:56:04 +0200

    Added new movement algorithm
    - Now motion is done by assigning the forward, side and turn components of motion by percentage
    
    Co-Authored-By: Ahmad Zaki <107484564+Ahmad-Zaki05@users.noreply.github.com>

--- The_Pythons_Arduino_Mega_Code/Nano_Functions.ino
+++ The_Pythons_Arduino_Mega_Code/Nano_Functions.ino
@@ -1,12 +1,68 @@
-void read_Nano(){
-//  robot_heading = 0;
-  if(Serial2.available())
+void read_Nano() {
+  //  robot_heading = 0;
+  if (Serial1.available())
   {
-    received_imu_angle = Serial2.readStringUntil('\n');
-    Serial.print("IMU received: ");
-//    Serial.println(received_imu_angle);
-    robot_heading = received_imu_angle.toInt();  
+    received_imu_angle = Serial1.readStringUntil('\n');
+    robot_heading = received_imu_angle.toInt();
     Serial.print("Robot heading: ");
     Serial.println(robot_heading);
+    float headCorr = abs(setpoint - robot_heading) * 0.02 * 100;
+    if (robot_heading < 8 && robot_heading > -8) {
+//      digitalWrite(13, HIGH);
+      //      Serial.println("LED ON");
+      turn = 0;
+    }
+    else {
+      if (robot_heading < -8)
+        turn = -headCorr;//-30
+      else
+        turn = headCorr;//30
+//      digitalWrite(13, LOW);
+      //      Serial.println("LED OFF");
+    }
+
+
   }
+}
+void new_read_Nano() {
+  //  robot_heading = 0;
+  //Serial.println(Serial1.available());
+  if (Serial1.available() > 0)
+  {
+    //received_imu_angle = Serial1.readStringUntil('\n');
+    //robot_heading = received_imu_angle.toInt();
+    robot_heading = Serial1.parseInt();
+    //Serial.println("fsbkfsbf");
+  }
+    Serial.print("Robot heading: ");
+    Serial.println(robot_heading);
+    float headCorr = abs(setpoint - robot_heading) * 0.02 * 100;
+    if (robot_heading < 8 && robot_heading > -8) {
+//      digitalWrite(13, HIGH);
+      //      Serial.println("LED ON");
+      turn = 0;
+    }
+    else {
+      if (robot_heading < -8)
+        turn = -headCorr;//-30
+      else
+        turn = headCorr;//30
+//      digitalWrite(13, LOW);
+      //      Serial.println("LED OFF");
+    }
+
+
+}
+
+void new_read_nano()
+{
+  if(digitalRead(20))
+    turn = 30;
+  if(digitalRead(21))
+    turn = -30;
+  if((digitalRead(21) == 0 && digitalRead(20) == 0) || (digitalRead(21) == 1 && digitalRead(20) == 1))
+    turn = 0;
+//  Serial.print(digitalRead(21));
+//  Serial.print(" ");
+//  Serial.println(digitalRead(20));
 }
--- The_Pythons_Arduino_Mega_Code/The_Pythons_Arduino_Mega_Code.ino
+++ The_Pythons_Arduino_Mega_Code/The_Pythons_Arduino_Mega_Code.ino
@@ -1,5 +1,6 @@
 #include <math.h>
 #include<Servo.h>
+#include <EEPROM.h>
 
 /*** Cytron pins ***/
 byte l_f_dir = 6;
@@ -11,7 +12,27 @@ byte r_b_dir = 2;
 byte r_b_pwm = 3;
 byte r_f_dir = 4;
 byte r_f_pwm = 5;
+
+
+#define Down_Left_Direction 8
+#define Down_Right_Direction 2
+#define Upper_Left_Direction 6
+#define Upper_Right_Direction 4
+#define Down_Left_PWM 9
+#define Down_Right_PWM 3
+#define Upper_Left_PWM 7
+#define Upper_Right_PWM 5
+
+long counter = 0;
+
+int side = 0 , forward = 0, turn = 0;
+
+int speeda, speedb, speedc, speedd, temp;
+int flag = 0 , flag2 = 0 , Speed2 = 0;
+
 /*******************/
+
+unsigned long long int timer;
 
 #define move_s 80
 
@@ -32,17 +53,44 @@ int ir_read_BL = 0;
 int ir_read_BR = 0;
 int ir_read_RR = 0;
 int ir_read_FL = 0;
+
+//// Striker (new motors)
+#define ir_0 A1 ////right back
+#define ir_1 A2 ////right front
+#define ir_2 A5 ////back right
+#define ir_3 A6 ////back left
+#define ir_4 A9 //// left back
+#define ir_5 A10 //// left front//
+#define ir_6 A11 ////front right 
+#define ir_7 A12 ////front left
+
+// Keeper (old motors)
+//#define ir_0 A10 ////right back
+//#define ir_1 A9 ////right front
+//#define ir_2 A12 ////back right//
+//#define ir_3 A5 ////back left
+//#define ir_4 A6 //// left back
+//#define ir_5 A1 //// left front
+//#define ir_6 A11 ////front right 
+//#define ir_7 A2 ////front left
+
+short ir_offsets[8][2];
+
+short ir_calibration_offset = 70;
+
+#define ir_calib_button 41
+
 /***************************/
 
 /*** Camera variables ***/
 char camera_char;
 String cam_angle_str = "S";
-short cam_angle;
+int cam_angle;
 /************************/
 
 /*** Nano variables ***/
 String received_imu_angle;
-short robot_heading = 0;
+int robot_heading = 0;
 /**********************/
 
 /*** PID variables ***/
@@ -66,9 +114,20 @@ Servo dribbler;
 
 void setup() {
   Serial.begin(115200);
-  //Serial1.begin(115200);
+  Serial1.begin(115200);
   Serial2.begin(115200);
   Serial3.begin(115200);
+
+
+  pinMode(Down_Left_Direction, OUTPUT);
+  pinMode(Down_Right_Direction, OUTPUT);
+  pinMode(Upper_Left_Direction, OUTPUT);
+  pinMode(Upper_Right_Direction, OUTPUT);
+  pinMode(Down_Left_PWM, OUTPUT);
+  pinMode(Down_Right_PWM, OUTPUT);
+  pinMode(Upper_Left_PWM, OUTPUT);
+  pinMode(Upper_Right_PWM, OUTPUT);
+
   pinMode(l_f_pwm, OUTPUT);
   pinMode(l_b_pwm, OUTPUT);
   pinMode(l_f_dir, OUTPUT);
@@ -96,29 +155,105 @@ void setup() {
   delay(2000);
 
   dribbler.write(40);
+  //turnDribbler(1);
 
   //  pinMode(Solenoid,OUTPUT);
   //
   //  Brushless.attach(12);
   //  Brushless.writeMicroseconds(2000);
   //  Brushless.writeMicroseconds(1000);
+  /*
+    for (byte i = 0; i < 50;) {
+      if (Serial3.available()) {
+        i++;
+        //Serial.print("in for 1\n");
+      }
+    }
+    for (byte i = 0; i < 50;) {
+      if (Serial2.available()) {
+        i++;
+        //Serial.print("in for 2\n");
+      }
+    }
+  */
 
-  for (byte i = 0; i < 50;) {
-    if (Serial3.available()) {
-      i++;
-      //Serial.print("in for 1\n");
-    }
-  }
-  for (byte i = 0; i < 50;) {
-    if (Serial2.available()) {
-      i++;
-      //Serial.print("in for 2\n");
-    }
-  }
+  pinMode(ir_0, INPUT);
+  pinMode(ir_1, INPUT);
+  pinMode(ir_2, INPUT);
+  pinMode(ir_3, INPUT);
+  pinMode(ir_4, INPUT);
+  pinMode(ir_5, INPUT);
+  pinMode(ir_6, INPUT);
+  pinMode(ir_7, INPUT);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+
+  ir_offsets[0][0] = readInt(0);
+  ir_offsets[0][1] = readInt(1);
+  ir_offsets[1][0] = readInt(2);
+  ir_offsets[1][1] = readInt(3);
+  ir_offsets[2][0] = readInt(4);
+  ir_offsets[2][1] = readInt(5);
+  ir_offsets[3][0] = readInt(6);
+  ir_offsets[3][1] = readInt(7);
+  ir_offsets[4][0] = readInt(8);
+  ir_offsets[4][1] = readInt(9);
+  ir_offsets[5][0] = readInt(10);
+  ir_offsets[5][1] = readInt(11);
+  ir_offsets[6][0] = readInt(12);
+  ir_offsets[6][1] = readInt(13);
+  ir_offsets[7][0] = readInt(14);
+  ir_offsets[7][1] = readInt(15);
+  forward = 0;
+  side = 0;
+  turn = 0;
+
 }
 void loop() {
-  read_Camera();
+  //  Serial.print("IR Calibration Button: ");
+  //  Serial.println(digitalRead(ir_calib_button));
+  if (digitalRead(ir_calib_button))
+    calibrate_IRs();
+// forward = 0;
+// side = 0;
+  read_Nano();//
+  new_new_read_Camera(); //
+
+//  read_IR();
+  //   if(is_line_front()){
+  //    forward = 0;
+  //    side = 30;
+  //   }
+  //   if(is_line_right()){
+  //    forward = -30;
+  //    side = 0;
+  //   }
+  //   if(is_line_back()){
+  //    forward = 0;
+  //    side = -30;
+  //   }
+  //  side = 50;
+  //  if (is_line_right()) {
+  //    side = 0;
+  //    motioneq();
+  //    delay(300);
+  //    side = -70;
+  //  }
+
+  //Serial1.flush();
+  //Serial.print(cam_angle);
+  //Serial.println(".");
+  //Serial.println(robot_heading);
+  out_line();//
+  motioneq();//
+  //read_Camera();
+  //  for (short i = 0; i < 360; i += 45) {
+  //    angular_Motion(i, 120);
+  //    delay(500);
+  //  }
   //  maintain_Heading();
-  //  read_Nano();
+  //read_Nano();
   //  angular_Motion(90, 120);
 }
